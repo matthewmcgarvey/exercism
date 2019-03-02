5de9c875de1090289f306755be9f647a936937e3
Matthew McGarvey <tnraptor14@hotmail.com>
2019-03-02 09:23:23 -0600

    (crystal) rna-transcription update

--- crystal/rna-transcription/src/rna_transcription.cr
+++ crystal/rna-transcription/src/rna_transcription.cr
@@ -1,7 +1,5 @@
 module RnaComplement
-  CONVERSION = {'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U'}
-
   def self.of_dna(strand)
-    strand.chars.map { |x| CONVERSION[x] }.join("")
+    strand.tr("CGTA", "GCAU")
   end
 end
