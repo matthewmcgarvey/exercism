625d3643e04908cd84fe5c79667e204eb51b1970
Matthew McGarvey <tnraptor14@hotmail.com>
2019-03-03 19:03:59 -0600

    trying out php

---
+++ php/hamming/hamming.php
@@ -0,0 +1,15 @@
+<?php
+
+function distance($a, $b)
+{
+    if (strlen($a) != strlen($b)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+    $distance = 0;
+    for ($i = 0; $i < strlen($a); $i++) {
+        if ($a[$i] != $b[$i]) {
+            $distance++;
+        }
+    }
+    return $distance;
+}
