ce85c11c948812de8459b03afa74be6509043bb6
Matthew McGarvey <tnraptor14@hotmail.com>
2019-03-07 19:38:23 -0600

    (elixir) bob update

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,27 +1,25 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      silence(input) -> "Fine. Be that way!"
-      shouting(input) && questioning(input) -> "Calm down, I know what I'm doing!"
-      questioning(input) -> "Sure."
-      shouting(input) -> "Whoa, chill out!"
+      silence?(input) -> "Fine. Be that way!"
+      shouting?(input) && questioning?(input) -> "Calm down, I know what I'm doing!"
+      questioning?(input) -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
       true -> "Whatever."
     end
   end
 
-  defp shouting(input) do
-    contains_letters(input) && String.upcase(input) == input
+  defp shouting?(input) do
+    upped = String.upcase(input)
+    downed = String.downcase(input)
+    input == upped && upped != downed
   end
 
-  defp questioning(input) do
+  defp questioning?(input) do
     String.ends_with?(input, "?")
   end
 
-  defp silence(input) do
+  defp silence?(input) do
     String.trim(input) == ""
-  end
-
-  defp contains_letters(input) do
-    Regex.match?(~r/\p{L}/, input)
   end
 end
