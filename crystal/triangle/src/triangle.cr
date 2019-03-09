fa642157ad5158e0f7c2e814091bf6d85f3e9f30
Matthew McGarvey <tnraptor14@hotmail.com>
2019-03-08 19:55:29 -0600

    (crystal) triangle update

--- crystal/triangle/src/triangle.cr
+++ crystal/triangle/src/triangle.cr
@@ -1,24 +1,20 @@
-
 class Triangle
-  @valid : Bool
   def initialize(@sides : Array(Int32))
-    @valid = sides[0] > 0 \
-      && sides[1] > 0 \
-      && sides[2] > 0 \
-      && sides[0] + sides[1] >= sides[2] \
-      && sides[1] + sides[2] >= sides[0] \
-      && sides[0] + sides[2] >= sides[1]
   end
 
   def equilateral? : Bool
-    @valid && @sides.to_set.size == 1
+    valid? && @sides.to_set.size == 1
   end
 
   def isosceles? : Bool
-    @valid && @sides.to_set.size <= 2
+    valid? && @sides.to_set.size <= 2
   end
 
   def scalene? : Bool
-    @valid && !isosceles?
+    valid? && !isosceles?
+  end
+
+  private def valid? : Bool
+    @sides.all?(&.>(0)) && @sides.permutations.all? { |(a, b, c)| a + b >= c }
   end
 end
