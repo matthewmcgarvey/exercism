dfd3281c6fbfcd2dff79ae5a3bf30fcfdda26808
Matthew McGarvey <tnraptor14@hotmail.com>
2019-03-11 20:10:45 -0500

    (go) raindrops update, map was the wrong choice

--- go/raindrops/convert.go
+++ go/raindrops/convert.go
@@ -4,18 +4,22 @@ import (
 	"strconv"
 )
 
-var factorsToSounds = map[int]string{
-	3: "Pling",
-	5: "Plang",
-	7: "Plong",
+type factorToSound struct {
+	factor int
+	sound  string
 }
+
+var factorsToSounds = [3]factorToSound{
+	factorToSound{3, "Pling"},
+	factorToSound{5, "Plang"},
+	factorToSound{7, "Plong"}}
 
 // Convert turns numbers into raindrops
 func Convert(num int) string {
 	sound := ""
-	for factor, s := range factorsToSounds {
-		if num%factor == 0 {
-			sound = sound + s
+	for _, fts := range factorsToSounds {
+		if num%fts.factor == 0 {
+			sound += fts.sound
 		}
 	}
 	if sound != "" {
