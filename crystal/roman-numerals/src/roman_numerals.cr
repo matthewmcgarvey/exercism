d8236e40f6e683fc1c72126794d3f68de132b949
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2019-03-27 17:35:19 -0500

    (crystal) roman numerals update

--- crystal/roman-numerals/src/roman_numerals.cr
+++ crystal/roman-numerals/src/roman_numerals.cr
@@ -1,26 +1,24 @@
 struct Int
   CONVERSIONS = {
-       1 => "I",
-       4 => "IV",
-       5 => "V",
-       9 => "IX",
-      10 => "X",
-      40 => "XL",
-      50 => "L",
-      90 => "XC",
-     100 => "C",
-     400 => "CD",
-     500 => "D",
-     900 => "CM",
     1000 => "M",
+    900 => "CM",
+    500 => "D",
+    400 => "CD",
+    100 => "C",
+    90 => "XC",
+    50 => "L",
+    40 => "XL",
+    10 => "X",
+    9 => "IX",
+    5 => "V",
+    4 => "IV",
+    1 => "I",
   }
 
   def to_roman : String
     return "" if self <= 0
 
-    num, roman = CONVERSIONS
-      .select { |k, v| k <= self }
-      .max_by { |k, v| k }
+    num, roman = CONVERSIONS.find({0, ""}) {|k,v| k <= self}
     roman + (self - num).to_roman
   end
 end
