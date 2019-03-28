0c7543e0a793253e50551341de86e7b4477e0168
Matthew McGarvey <matthew.mcgarvey@daveramsey.com>
2019-03-27 19:35:51 -0500

    (crystal) forth update

--- crystal/forth/src/forth.cr
+++ crystal/forth/src/forth.cr
@@ -1,74 +1,102 @@
 module Forth
+  extend self
+
   OPERATIONS = {
-    "+" => ->sum(Array(Int32)),
-    "-" => ->diff(Array(Int32)),
-    "*" => ->product(Array(Int32)),
-    "/" => ->division(Array(Int32)),
-    "dup" => ->dup(Array(Int32)),
+    "+"    => ->add(Array(Int32)),
+    "-"    => ->sub(Array(Int32)),
+    "*"    => ->mult(Array(Int32)),
+    "/"    => ->div(Array(Int32)),
+    "dup"  => ->dup(Array(Int32)),
     "drop" => ->drop(Array(Int32)),
     "swap" => ->swap(Array(Int32)),
     "over" => ->over(Array(Int32)),
   }
 
-  def self.evaluate(input) : Array(Int32)
-    actual = inline_definitions(input.downcase)
-    actual.split(" ")
-      .reduce([] of Int32) do |acc, x|
-        x.to_i?.try { |num| acc << num } \
-          || OPERATIONS[x]?.try(&.call(acc)) \
-          || raise Exception.new
+  def evaluate(input) : Array(Int32)
+    definitions, program = prepare(input)
+    user_defined_ops = compile(definitions)
+    program.split(" ")
+      .reduce([] of Int32) do |stack, token|
+        methodize(token, stack, user_defined_ops) || bar(stack, token)
       end
   end
 
-  private def self.inline_definitions(input): String
-    res = input.scan(/:(.*?);/)
-    return input unless res
+  private def bar(stack : Array(Int32), token : String) : Array(Int32)
+    numerize(token, stack) || methodize(token, stack, OPERATIONS) || raise Exception.new
+  end
 
-    captures = input.scan(/:(.*?);/).map(&.[1])
-    reg = input.rpartition(";")[-1]
-    definitions = captures.reject(&.nil?)
-      .map(&.as(String))
-      .map(&.strip.split(" "))
-      .map {|x| {x[0], x[1..-1].join(" ")}}
+  private def numerize(token, stack)
+    token.to_i?.try { |num| stack << num }
+  end
+
+  private def methodize(token, stack, ops)
+    ops[token]?.try(&.call(stack))
+  end
+
+  private def prepare(input)
+    temp = input.downcase.split(";")
+    {temp[0..-2], temp[-1]}
+  end
+
+  private def compile(definitions : Array(String))
+    definitions
+      .map { |definition| definition.strip.split(" ")[1..-1] }
+      .map { |parts| {parts[0], define_user_op(parts)} }
       .to_h
-    raise Exception.new if definitions.map(&.[0]).any?(&.to_i?)
-    definitions.reduce(reg) {|prog, (k,v)| prog.gsub(k, v)}
   end
 
-  private def self.sum(nums)
-    raise Exception.new if nums.size < 2
-    [nums.sum]
+  private def define_user_op(x) : Proc(Array(Int32), Array(Int32))
+    ->(stack : Array(Int32)) do
+      x[1..-1].reduce(stack) { |acc, token| token; bar(acc, token) }
+    end
   end
 
-  private def self.diff(nums)
-    raise Exception.new if nums.size < 2
-    [nums.reduce {|acc, curr| acc - curr }]
+  private def add(nums)
+    arr, a, b = pop_2(nums)
+    arr << a + b
   end
 
-  private def self.product(nums)
-    raise Exception.new if nums.size < 2
-    [nums.product]
+  private def sub(nums)
+    arr, a, b = pop_2(nums)
+    arr << a - b
   end
 
-  private def self.division(nums)
-    raise Exception.new if nums.size < 2
-    [nums.reduce {|acc, curr| acc / curr }]
+  private def mult(nums)
+    arr, a, b = pop_2(nums)
+    arr << a * b
   end
 
-  private def self.dup(nums)
-    nums << nums.last
+  private def div(nums)
+    arr, a, b = pop_2(nums)
+    arr << a / b
   end
 
-  private def self.drop(nums)
-    raise Exception.new if nums.empty?
-    nums[0..-2]
+  private def dup(nums)
+    _, last = pop(nums)
+    nums << last
   end
 
-  private def self.swap(nums)
-    nums[0..-3] << nums[-1] << nums[-2]
+  private def drop(nums)
+    pop(nums)[0]
   end
 
-  private def self.over(nums)
-    nums << nums[-2]
+  private def swap(nums)
+    arr, a, b = pop_2(nums)
+    arr << b << a
+  end
+
+  private def over(nums)
+    arr, a, _ = pop_2(nums)
+    nums << a
+  end
+
+  private def pop(nums)
+    {nums[0..-2], nums[-1]}
+  end
+
+  private def pop_2(nums)
+    arr, b = pop(nums)
+    arr, a = pop(arr)
+    {arr, a, b}
   end
 end
